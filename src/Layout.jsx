679301c16c74da7c1e04cf0758d18e0a1a93ea19
Vasterz <mrfernandobritos@gmail.com>
2025-11-17 03:58:04 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -265,8 +265,8 @@ export default function Layout({ children }) {
               </svg>
             </a>
             
-            <a href="https://www.tiktok.com/@TLIUStudios" target="_blank" rel="noopener noreferrer" className="text-slate-500 dark:text-slate-500 hover:text-[#00f2ea] dark:hover:text-[#00f2ea] transition-colors">
-              <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <a href="https://www.tiktok.com/@TLIUStudios" target="_blank" rel="noopener noreferrer" className="inline-flex text-slate-500 dark:text-slate-500 hover:text-[#00f2ea] dark:hover:text-[#00f2ea] transition-colors">
+              <svg className="w-5 h-5 transform-none" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
               </svg>
             </a>
