ff60a4f8c7f74ba228e3ba6bd463f4d3fc0a4a82
Vasterz <mrfernandobritos@gmail.com>
2025-12-06 12:53:32 +0000

    File changes

--- src/components/comparison/ImageComparisonModal.jsx
+++ src/components/comparison/ImageComparisonModal.jsx
@@ -1020,7 +1020,7 @@ export default function ImageComparisonModal({
                 <div className="flex-1 relative w-full flex items-center justify-center py-4">
                   <div
                     ref={imageContainerRef}
-                    className="relative"
+                    className="relative inline-block"
                     style={{
                       transform: `scale(${zoom}) translate(${pan.x / zoom}px, ${pan.y / zoom}px)`,
                       transformOrigin: 'center',
@@ -1048,20 +1048,20 @@ export default function ImageComparisonModal({
                       alt="Compressed"
                       className="max-w-[85vw] lg:max-w-[60vw] max-h-[calc(100vh-200px)] block"
                       draggable="false"
-                      style={{ width: 'auto', height: 'auto' }}
+                      style={{ width: 'auto', height: 'auto', display: 'block' }}
                     />
 
-                    <div 
-                      className="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none"
-                      style={{ clipPath: `inset(0 ${100 - sliderPosition}% 0 0)` }}
-                    >
-                      <img
-                        src={originalImage}
-                        alt="Original"
-                        className="w-full h-full object-cover"
-                        draggable="false"
-                      />
-                    </div>
+                    <img
+                      src={originalImage}
+                      alt="Original"
+                      className="absolute top-0 left-0 max-w-[85vw] lg:max-w-[60vw] max-h-[calc(100vh-200px)] block pointer-events-none"
+                      draggable="false"
+                      style={{ 
+                        width: 'auto', 
+                        height: 'auto', 
+                        clipPath: `inset(0 ${100 - sliderPosition}% 0 0)` 
+                      }}
+                    />
 
                     {zoom === 1 && !isPanning && (
                       <div
