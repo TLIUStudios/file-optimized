6e5c3cfd431c02c24a72bf80587f715dcd85edb2
TLIU Studios <mrfernandobritos@gmail.com>
2026-01-08 01:33:20 +0000

    File changes

--- src/components/upload/MediaCard.jsx
+++ src/components/upload/MediaCard.jsx
@@ -2830,8 +2830,8 @@ export default function MediaCard({ image, onRemove, onProcessed, onCompare, aut
                       </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent className="w-56">
-                      <DropdownMenuItem onClick={handleSaveToGoogleDrive} disabled={uploadingToDrive}>
-                        <svg className="w-4 h-4 mr-2" viewBox="0 0 87.3 78">
+                      <DropdownMenuItem disabled className="opacity-50 cursor-not-allowed">
+                        <svg className="w-4 h-4 mr-2 opacity-40" viewBox="0 0 87.3 78">
                           <path fill="#0066da" d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"/>
                           <path fill="#00ac47" d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z"/>
                           <path fill="#ea4335" d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z"/>
@@ -2839,8 +2839,8 @@ export default function MediaCard({ image, onRemove, onProcessed, onCompare, aut
                           <path fill="#2684fc" d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z"/>
                           <path fill="#ffba00" d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z"/>
                         </svg>
-                        <span className="flex-1">Google Drive</span>
-                        <Badge className="bg-emerald-600 text-white text-[10px]">Active</Badge>
+                        <span className="flex-1 text-slate-400">Google Drive</span>
+                        <Badge variant="outline" className="text-[10px]">Coming Soon</Badge>
                       </DropdownMenuItem>
                       <DropdownMenuItem disabled className="opacity-50 cursor-not-allowed">
                         <svg className="w-4 h-4 mr-2 opacity-40" viewBox="0 0 24 24" fill="#0061FF">
