dba430f6a50e70873adae7b896d4992c463b12da
TLIU Studios <mrfernandobritos@gmail.com>
2026-03-01 10:34:00 +0000

    File changes

--- src/components/upload/GLBCard.jsx
+++ src/components/upload/GLBCard.jsx
@@ -199,7 +199,7 @@ export default function GLBCard({ file, onRemove, onProcessed }) {
         >
           <X className="w-4 h-4" />
         </Button>
-      </div>
+        </div>
 
       <div className="p-3 sm:p-4 space-y-3 sm:space-y-4">
         <div>
